6580540dbdda158091c9878cfee54b54f66da60d
Jonas Robertsson <jonas.robertsson@icloud.com>
2026-09-01 17:28:25 +0200

    code-buster: update formula to 0.3.0

--- Formula/code-buster.rb
+++ Formula/code-buster.rb
@@ -4,11 +4,11 @@ class CodeBuster < Formula
   license "MIT"
 
   if OS.mac?
-    url "https://github.com/tool-bunker/code-buster/releases/download/v0.2.0/code-buster-macos-arm64.tar.gz"
-    sha256 "db5e8813ba9d0abd950750c230978596f791d72441ef88cd6ce93630c655e768"
+    url "https://github.com/tool-bunker/code-buster/releases/download/v0.3.0/code-buster-macos-arm64.tar.gz"
+    sha256 "12e879cf6f61cf6a54457bf5f20455810cbc3484f940684d63e96b2dbdfac570"
   else
-    url "https://github.com/tool-bunker/code-buster/releases/download/v0.2.0/code-buster-linux-x64.tar.gz"
-    sha256 "c787e55136bf327bb42a361ef7650ad4868082e83919206f0b12f96c9007d5e9"
+    url "https://github.com/tool-bunker/code-buster/releases/download/v0.3.0/code-buster-linux-x64.tar.gz"
+    sha256 "4259ad721cb66b52cd56f8a7f196168aed12bec2057c08ae65e7c199fbfb72f3"
   end
 
   depends_on arch: :arm64 if OS.mac?
@@ -19,6 +19,6 @@ class CodeBuster < Formula
   end
 
   test do
-    assert_match "cb 0.2.0", shell_output("#{bin}/cb version")
+    assert_match "cb 0.3.0", shell_output("#{bin}/cb version")
   end
 end
